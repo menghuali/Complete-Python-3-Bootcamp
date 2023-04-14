992d24377aa0d8d7d22b291c1d7d009f216dfabe
Menghua Li <menghua.li.1980@gmail.com>
2023-04-14 17:14:52 -0400

    Moved MyWorkspace

--- MyWorkspace/chap09_module_import/MainPackage/SubPackage/sub_script.py
+++
@@ -1,2 +0,0 @@
-def report_sub():
-    print("Hey, I am a function in sub_script in sub package")
--- MyWorkspace/chap09_module_import/MainPackage/main_script.py
+++
@@ -1,2 +0,0 @@
-def report_main():
-    print("Hey, I am a function in main script in main package")
